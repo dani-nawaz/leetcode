d4a8fd2cca0a09a6b392feed0b11d4e763d41a97
Abdul Rehman Nawaz <69709106+dani-nawaz@users.noreply.github.com>
2025-09-29 01:06:37 -0700

    Time: 136 ms (19.04%) | Memory: 34.8 MB (13.75%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+
+        for index, value in enumerate(temperatures):
+            while stack and value > stack[-1][1]:
+                stackIndex, stackValue = stack.pop()
+                res[stackIndex] = index - stackIndex
+
+
+            stack.append([index, value])
+
+        return res
+        
