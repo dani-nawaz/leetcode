8e1417d8cd0317edb22e074b9a6c97f8d7e19d36
Abdul Rehman Nawaz <69709106+dani-nawaz@users.noreply.github.com>
2025-10-06 13:29:41 +0500

    Time: 387 ms (20.43%) | Memory: 41 MB (13.13%) - LeetSync

---
+++ 883-car-fleet/car-fleet.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+
+        pair = sorted([[p, s] for p, s in zip(position, speed)], reverse = True)
+
+
+        stack = []
+
+        for p, s in pair:
+            stack.append((target - p) / float(s))
+
+            if len(stack) >=2 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+
+        return len(stack)
+        
